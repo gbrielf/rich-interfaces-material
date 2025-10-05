7e69b2e02abb01059020dfb146f1bbc11a5c9ee3
Gabriel Henrique F. Barbosa <56904537+gbrielf@users.noreply.github.com>
2025-10-05 21:02:13 +0000

    feat: pushing question 4

---
+++ 1-classes-and-functions-in-Typescript/question-4.js
@@ -0,0 +1,8 @@
+// 4. Create a program that reads the array and takes only the first two elements. Use the SLICE method of the Array class.
+// 4.1 Write a test with the array [2,4,6,2,8,9,5]
+// 4. Faça um programa que leia o array pegue apenas os dois primeiros elementos. Use o método SLICE da classe Array.
+// 4.1 Escreva um teste com o array [2,4,6,2,8,9,5]
+var array = [2, 4, 6, 2, 8, 9, 5];
+function slicer(x) { return x.slice(0, 2); }
+var twoFirstElements = slicer(array);
+console.log(twoFirstElements);
