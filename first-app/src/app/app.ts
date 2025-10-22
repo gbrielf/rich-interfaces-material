fa1d0b1fd969decfcf26931374b926f484008116
Gabriel Henrique F. Barbosa <56904537+gbrielf@users.noreply.github.com>
2025-10-22 11:29:30 +0000

    feat: reiniciando criacao do layout

--- first-app/src/app/app.ts
+++ first-app/src/app/app.ts
@@ -1,24 +1,66 @@
 import { Component, signal } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { PanelModule } from 'primeng/panel';
+import { InputTextModule } from 'primeng/inputtext';
+import { SplitterModule } from 'primeng/splitter';
+import { MessageModule } from 'primeng/message';
 
 @Component({
   selector: 'app-root',
-  imports: [ ButtonModule, PanelModule],
+  imports: [ButtonModule, PanelModule, InputTextModule, SplitterModule, MessageModule],
   // url responsible for the component
   // templateUrl: './app.html',
   // "inline" template of the component
   template: `
-      <h1>Hello, {{ title() }}</h1>
-      <p>Congratulations! Your app is running. 🎉</p>
-
-      <p-panel header="Header">
+    <!-- <h1>Hello, {{ title() }}</h1>
+      <p>Congratulations! Your app is running. 🎉</p> -->
+    <!-- <p-panel header="Header">
           <p class="m-0">
               Valor do contador {{ counter }}
           </p>
             <p-button label="Incrementar 1" (onClick)='incrementar()' />
-      </p-panel>
-    `,
+      </p-panel> -->
+    <p-splitter [style]="{ height: '300px' }" class="flex align-items justify-center mb-8">
+      <ng-template #panel>
+        <h1 class="text-3xl font-bold underline">Logotipo</h1>
+      </ng-template>
+      <ng-template #panel>
+        <!-- [formGroup]="exampleForm"
+        (ngSubmit)="onSubmit()" -->
+        <form class="flex flex-col gap-4 w-full sm:w-56">
+          <div class="flex flex-col gap-1">
+            <input
+              pInputText
+              type="text"
+              id="username"
+              placeholder="Username"
+              formControlName="username"
+            />
+            <!-- [invalid]="isInvalid('username')" -->
+            <!-- @if (isInvalid('username')) {
+            <p-message severity="error" size="small" variant="simple"
+              >Username is required.</p-message
+            >
+            } -->
+          </div>
+          <div class="flex flex-col gap-1">
+            <!-- [invalid]="isInvalid('email')" -->
+            <input pInputText type="email" id="email" placeholder="Email" formControlName="email" />
+            <!-- @if (isInvalid('email')) { @if (exampleForm.get('email')?.errors?.['required']) {
+            <p-message severity="error" size="small" variant="simple">Email is required.</p-message>
+            } @if (exampleForm.get('email')?.errors?.['email']) {
+            <p-message severity="error" size="small" variant="simple"
+              >Please enter a valid email.</p-message
+            >
+            } } -->
+          </div>
+          <button pButton severity="secondary" type="submit">
+            <span pButtonLabel>Submit</span>
+          </button>
+        </form>
+      </ng-template>
+    </p-splitter>
+  `,
   // css do componente,
   styleUrl: './app.css',
 })
